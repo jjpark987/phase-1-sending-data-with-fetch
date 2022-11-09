1be4ada231f138cb0e110989f6e15674035aba53
Jason Park <jjpark987@gmail.com>
2022-11-09 15:53:44 -0800

    commit tree 093edee05a492208f387c1a5dc6899aa68a6b6f3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let userInfoObj = {
+        name: userName,
+        email: userEmail
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfoObj)
+    })
+    .then(response => response.json())
+    .then(userInfo => document.body.innerHTML = userInfo.id)
+    .catch(error => document.body.innerHTML = error)
+}
